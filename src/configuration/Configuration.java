a11d904af7243a90efe5082ed4b8f4bc6d7d13d1
rafiibro <rafael.dacunhagarcia@icloud.com>
2019-04-04 09:35:45 +0200

    Creation des packages, commencement des mails

--- src/configuration/Configuration.java
+++ src/configuration/Configuration.java
@@ -1,3 +1,5 @@
+package configuration;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
---
+++ src/mails/Mail.java
@@ -0,0 +1,8 @@
+package mails;
+
+public class Mail {
+    public String from = "from: ";
+    public String to = "to: ";
+    public String subject = "subject: ";
+    public String mail = "\n";
+}
